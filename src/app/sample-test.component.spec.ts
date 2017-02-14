75e62d40c8028849af31838e9a15441d76c1a734
wattamoves00 <kpanuncio123@yahoo.com>
2017-02-15 00:38:17 +0800

    work on async

--- src/app/sample-test.component.spec.ts
+++ src/app/sample-test.component.spec.ts
@@ -1,25 +1,48 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { SampleTestComponent } from './sample-test.component';
 import { UserService } from './users.service';
 
 describe('Aync Unit Test', () => {
+
+    let comp: SampleTestComponent;
+    let fixture: ComponentFixture<SampleTestComponent>;
+    let spy: jasmine.Spy;
+    let el: HTMLElement;
+    let de: DebugElement;
+    let userService: UserService;
+
+    const testUser = 'User';
+
     beforeEach( () => {
         TestBed.configureTestingModule({
             declarations: [ SampleTestComponent ],
             providers: [ UserService ]
         });
+
+        fixture = TestBed.createComponent( SampleTestComponent );
+        comp = fixture.componentInstance;
+        userService = fixture.debugElement.injector.get( UserService );
+        spy = spyOn( userService, 'getUsers' )
+                    .and.returnValue(Promise.resolve(testUser));
         
+        de = fixture.debugElement.query(By.css('.user'));
+        el = de.nativeElement;
     });
 
-    let fixture = TestBed.createComponent( SampleTestComponent );
-    let comp = fixture.componentInstance;
-    let userService = fixture.debugElement.injector.get( UserService );
-    let spy = spyOn( userService, 'getUsers' )
-                .and.returnValue(Promise.resolve('user'));
+    it('should not show user before OnInit', () => {
+        expect(el.textContent).toBe('', 'no user to be displayed')
+        expect(spy.calls.any()).toBe(false, 'getUser method not called yet');
+    });
 
-    it('should get the user name', () => {
+    it('should show the user name after promise (async)', async(() => {
         fixture.detectChanges();
-        expect(spy.calls.any()).toBe('Steve', 'expected name');
-    })
+
+        fixture.whenStable().then(() => {
+            fixture.detectChanges();
+            expect(el.textContent).toBe(testUser);
+        });
+    }));
 
 });
--- src/app/sample-test.component.ts
+++ src/app/sample-test.component.ts
@@ -3,7 +3,7 @@ import { UserService } from './users.service';
 
 @Component({
     selector: 'sample-page',
-    template: '<p>Welcome, {{user}}!</p>'
+    template: '<p>Welcome, <b class="user">{{user}}</b>! </p>'
 })
 
 export class SampleTestComponent implements OnInit {
